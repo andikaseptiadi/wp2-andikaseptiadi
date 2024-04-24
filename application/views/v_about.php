19e485558c8e54a06f8a894806f4a9ac36e1383b
andikaseptiadi <andikaseptiadi71@gmail.com>
2024-04-24 08:18:32 +0700

    pertemuan 4 dan tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+<h1><?php echo $judul ?></h1>
+51
+<h4>Nama</h4>
+<ul type="disc">
+<li>Nama Depan : Andika </li>
+<li>Nama Belakang : Septiadi</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+<li> Jalan Tanjung Raya 2 Gg Nusa Karya</li>
+</ul>
+<h4>Tempat Lahir</h4>
+<ul type="none">
+<li>Pontianak</li>
+</ul>
+<h4>Olah Raga Favorit</h4>
+<ul type="square">
+<li>Bulutangkis</li>
+<li>Futsal</li>
+</ul>
+</section>
